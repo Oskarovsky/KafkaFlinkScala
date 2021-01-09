b5cd25db75bdca9c1e65bec88e58e7af99e82f02
oskarro <oskar.slyk@gmail.com>
2021-01-09 18:07:11 +0100

    kafka producer

--- src/main/scala/com/oskarro/MainProducer.scala
+++ src/main/scala/com/oskarro/MainProducer.scala
@@ -5,13 +5,10 @@ import net.liftweb.json.JsonParser.parse
 import net.liftweb.json.Serialization.write
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import play.api.libs.json.Json
-import akka.actor.Actor
-import akka.actor.Props
 
 import java.text.SimpleDateFormat
-import scala.concurrent.duration._
 import java.util.{Calendar, Properties}
-import java.util.concurrent.ScheduledThreadPoolExecutor
+import scala.concurrent.duration._
 
 object MainProducer {
 
@@ -22,14 +19,17 @@ object MainProducer {
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-  val apiKey: String = "b168711-aefd-4825-973a-4e1526c6ce93"
+  val apiKey: String = "3b168711-aefd-4825-973a-4e1526c6ce93"
   val resourceID: String = "2e5503e-927d-4ad3-9500-4ab9e55deb59"
 
   def main(args: Array[String]): Unit = {
     val system = akka.actor.ActorSystem("system")
     import system.dispatcher
-    system.scheduler.schedule(10 seconds, 10 seconds) {
+    system.scheduler.schedule(2 seconds, 10 seconds) {
       produceCurrentLocationOfVehicles("bus")
+    }
+    system.scheduler.schedule(6 seconds, 15 seconds) {
+      produceCurrentLocationOfVehicles("tram")
     }
   }
 
@@ -47,11 +47,11 @@ object MainProducer {
 
     val now = Calendar.getInstance().getTime
     val dataFormat = new SimpleDateFormat("yyyy-MM-dd  hh:mm:ss")
-    println(s"[Timestamp - ${dataFormat.format(now)}] -- JSON Data for $vehicleType parsing started.")
+    println(s"[Timestamp - ${dataFormat.format(now)}] JSON Data for $vehicleType parsing started.")
     val req = requests.get("https://api.um.warszawa.pl/api/action/busestrams_get/",
       params = Map(
-        "resource_id" -> "2e5503e-927d-4ad3-9500-4ab9e55deb59",
-        "apikey" -> "3b168711-aefd-4825-973a-4e1526c6ce93",
+        "resource_id" -> resourceID,
+        "apikey" -> apiKey,
         "type" -> vehicleTypeNumber))
 
     val jsonObjectFromString = Json.parse(req.text)
@@ -59,7 +59,7 @@ object MainProducer {
 
     implicit val formats: DefaultFormats.type = DefaultFormats
     val vehicleList = parse(response.get.toString()).extract[List[BusStream]]
-    vehicleList foreach {veh => KafkaProducer.writeToKafka("VEHICLES_ACTION","temat_oskar01", Main.props, write(veh))}
+    vehicleList foreach {veh => KafkaProducer.writeToKafka(s"[PROCESS: $vehicleType localization]","temat_oskar01", Main.props, write(veh))}
 
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
