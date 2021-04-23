1bf60e0a26369a908794807f72c0bb2d63f5cc01
Oskarovsky <oskar.slyk@gmail.com>
2021-04-24 01:18:24 +0200

    fli

--- src/main/scala/com/oskarro/flink/MainConsumer.scala
+++ src/main/scala/com/oskarro/flink/MainConsumer.scala
@@ -4,15 +4,20 @@ import com.oskarro.configuration.Constants
 import com.oskarro.model.BusModel
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.api.scala.createTypeInformation
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.api.windowing.assigners.{TumblingEventTimeWindows, TumblingProcessingTimeWindows}
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.streaming.connectors.cassandra.CassandraSink
 import org.apache.flink.streaming.connectors.cassandra.CassandraSink.CassandraSinkBuilder
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
+import org.apache.flink.streaming.api.scala.function.{ProcessWindowFunction, WindowFunction}
+import org.apache.flink.util.Collector
 import org.json4s
 import org.json4s.native.JsonMethods
 import play.api.libs.json.{Json, Reads}
 
-import java.text.SimpleDateFormat
+import java.lang
 import java.util.Properties
 
 object MainConsumer {
@@ -31,19 +36,41 @@ object MainConsumer {
     val kafkaConsumer = new FlinkKafkaConsumer011[String](topic, new SimpleStringSchema(), properties)
     kafkaConsumer.setStartFromLatest()
 
-    val busDataStream = env.addSource(kafkaConsumer)
+    val busDataStream: DataStream[BusModel] = env.addSource(kafkaConsumer)
       .flatMap(raw => JsonMethods.parse(raw).toOption)
       .map(_.extract[BusModel])
 
+    busDataStream.print()
+
     createTypeInformation[(String, Long, String, Long, String, Long, String)]
 
-    val xxx = env.addSource(kafkaConsumer).flatMap {
-      raw =>
-        JsonMethods.parse(raw).toOption
-          .map(_.extract[BusModel])
+    /* JSON END REGION */
+
+
+
+/*    // a common operator to process different aggregation
+    class CustomCountProc() extends ProcessWindowFunction[BusModel, BusModel, String, TimeWindow] {
+
+      override def process(key: String, context: Context, elements: Iterable[BusModel], out: Collector[BusModel]): Unit = {
+        print("AAAAA")
+        for (e <- elements) {
+          print("AAAAA")
+          out.collect(BusModel(e.Lines, e.Lon, e.VehicleNumber, e.Time, e.Lat, e.Brigade))
+        }
+
+      }
     }
 
+    busDataStream
+      .keyBy(_.Brigade)
+      .timeWindow(Time.seconds(10))
+      .process(new CustomCountProc)
+      .print()*/
 
+    env.execute("Flink Kafka Example")
+
+
+    /* CASSANDRA */
     // Creating bus data to sink into cassandraDB.
     val sinkBusDataStream = busDataStream
       .map(
@@ -74,13 +101,7 @@ object MainConsumer {
         " values (?, ?, ?, ?, ?, ?, ?);")
       .build()
 
-/*    busDataStream.map(x => {
-      val str = Json.parse(x)
-      println(str)
-      implicit val formats: DefaultFormats.type = DefaultFormats
-    })*/
-
-
     env.execute("Flink Kafka Example")
   }
+
 }
